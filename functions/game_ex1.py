26060bcc421cbefc68bc2ed7ad59cd1d9f231f60
Pranshirastogi <rastogipranshi65@gmail.com>
2022-11-24 18:38:56 +0530

    practice game example

--- functions/game_ex1.py
+++ functions/game_ex1.py
@@ -5,10 +5,20 @@ WIDTH = 800
 
 p = Actor('ironman',(100,100))
 c = Actor('cookie_img')
+    
 
 def draw():
     screen.fill('white')
     p.draw()
     c.draw()
+    print('drawing')
+
+def update():
+    print('updating')
+    p.x -= 3
+    p.angle = -10
+    if p.x < 0: # agar player left side se bahar jaae
+        p.X = WIDTH
+    print(p.x,p.y)
 
 pgzrun.go()
