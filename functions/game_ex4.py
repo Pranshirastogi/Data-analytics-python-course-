5f2b455ea78cfece9d69b207f0ad6044616346e6
Pranshirastogi <rastogipranshi65@gmail.com>
2022-11-29 18:36:22 +0530

    game practice

--- functions/game_ex4.py
+++ functions/game_ex4.py
@@ -1,44 +1,30 @@
 import pgzrun
-from random import randint
 
-HEIGHT = 600
 WIDTH = 1200
+HEIGHT = 600
 
-p = Actor('ironman',center=(WIDTH//2,HEIGHT//2))
-c = Actor('cookie_img', (randint(0,WIDTH), randint(0,HEIGHT)))
-title = "IRON-MAN GAME"
-score = 0
-music.play('bg')
-music.set_volume(0.2)
+scr = 0
+
+def gamescr(title,bgcolor='gray',info='Play the game'):
+        screen.fill(bgcolor)
+        screen.draw.text(title,center=(WIDTH/2,HEIGHT/2), fontsize=100,color='white',align='center')
+        screen.draw.text(info, center=(WIDTH/2,HEIGHT/2+100),fontsize=50,color='white', align='center')
 
 def draw():
-    screen.fill('white')
-    screen.draw.text(title,center=(WIDTH//2,30), fontsize=60,color='black', align='center',shadow=(.2,1),scolor='red')
-    screen.draw.text(f'score:{score}', (10,10), fontsize = 30, color = 'black')
-    p.draw()
-    c.draw()
+    if scr==0:
+        gamescr('Amazing game','black','Press space to start')
+    elif scr ==1:
+        gamescr(bgcolor='blue',title='Game is running')
+    elif scr ==2:
+        gamescr('GAME OVER',info='GO HOME')
 
-def p_move():
-    if keyboard.right and p.right < WIDTH:
-        p.x +=3
-        p.angle = -10
-    elif keyboard.left and p.left > 0:
-        p.x -=3
-        p.angle = 10
-    else:
-        p.angle = 0
-    if keyboard.up and p.top > 0:
-        p.y -=3
-    if keyboard.down and p.bottom < HEIGHT:
-        p.y +=3
-    
 def update():
-    global score # tells python, that we want to change the value of the global variable score
-    p_move() 
-    if p.colliderect(c):
-        score +=1
-        c.pos = (randint(0,WIDTH), randint(0,HEIGHT))
-        sounds.s1.play()
-    print(p.x,p.y,p.angle) # fuction call
-    
+    global scr
+    if keyboard.space and scr == 0:
+        scr=1
+    if keyboard.escape and scr == 1:
+        scr = 2
+    if keyboard.RETURN and scr == 2:
+        scr = 0
+
 pgzrun.go()
